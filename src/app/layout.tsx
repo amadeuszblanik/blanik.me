42bde4291193ffcb943ff56b25eed35d7ddca0e5
Amadeusz Blanik <amadeusz@blanik.me>
2024-02-09 23:57:11 +0100

    Added assets and meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import Providers from "@/app/providers";
 import React from "react";
 import { BmeFooter } from "@/lib/components";
 import styled from "styled-components";
-import { usePathname } from "next/navigation";
 
 const PositionWrapper = styled.div`
   display: flex;
@@ -20,11 +19,21 @@ export default function Layout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const pathname = usePathname();
-
   return (
     <html lang="en">
-      <body className={pathname === "/" ? "scheme-dark bg-gradient" : "scheme-light"}>
+      <title>Amadeusz Blanik</title>
+      <meta
+        name="description"
+        content="I am a Frontend Developer with commercial experience. My passion is my work even after hours I used to develop and learn new things. I'm available to hire!"
+      />
+      <meta name="keywords" content="front-end developer amadeusz blanik frontend" />
+      <meta name="author" content="Amadeusz Blanik" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/meta/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/meta/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/meta/favicon-16x16.png" />
+      <link rel="icon" href="/meta/favicon.ico" />
+      <body>
         <Providers>
           <PositionWrapper>
             <div>{children}</div>
