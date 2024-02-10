cee9e47ade41f80cb2224be58d3f7bab2bc5e26d
Amadeusz Blanik <amadeusz@blanik.me>
2024-02-10 02:41:40 +0100

    Fixed time formatter

--- src/lib/service/time-formatter.ts
+++ src/lib/service/time-formatter.ts
@@ -2,7 +2,7 @@ export default class Service {
   constructor(public date: Date) {}
 
   mediumLocal(): string {
-    return this.date.toLocaleDateString("en-GB", {
+    return this.date.toLocaleTimeString("en-GB", {
       timeZone: "Europe/Warsaw",
       hour: "2-digit",
       minute: "2-digit",
