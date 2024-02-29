b4e25910bb397baebb8ac03eb87a76a6c7649435
Amadeusz Blanik <amadeusz@blanik.me>
2024-02-29 02:05:12 +0100

    Added stylesheet manager

--- src/lib/components/providers.tsx
+++ src/lib/components/providers.tsx
@@ -1,18 +1,23 @@
 import React, { PropsWithChildren } from "react";
-import { ThemeProvider } from "styled-components";
+import { StyleSheetManager, ThemeProvider } from "styled-components";
 import { ConfigStore } from "@/lib/context";
 import { ConfigContext } from "../context/config.context";
 import { ThemeVariants } from "@/lib/model/theme-variants.model";
 import { themeDark, themeLight } from "@/styles";
+import isPropValid from "@emotion/is-prop-valid";
 
-const Component: React.FC<PropsWithChildren> = ({ children }) => (
-  <ConfigStore>
-    <ConfigContext.Consumer>
-      {({ theme }) => (
-        <ThemeProvider theme={theme === ThemeVariants.Dark ? themeDark : themeLight}>{children}</ThemeProvider>
-      )}
-    </ConfigContext.Consumer>
-  </ConfigStore>
-);
+const Component: React.FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <StyleSheetManager shouldForwardProp={(propName) => isPropValid(propName)}>
+      <ConfigStore>
+        <ConfigContext.Consumer>
+          {({ theme }) => (
+            <ThemeProvider theme={theme === ThemeVariants.Dark ? themeDark : themeLight}>{children}</ThemeProvider>
+          )}
+        </ConfigContext.Consumer>
+      </ConfigStore>
+    </StyleSheetManager>
+  );
+};
 
 export default Component;
